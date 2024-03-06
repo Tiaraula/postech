0205d98948fe14eedcefb570b0fe64a065d5cbe1
tiaraadyaa <tiaradhiyaa03@gmail.com>
2024-03-06 16:29:39 +0700

    Pertemuan 3

---
+++ database/migrations/2024_03_06_070824_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->Integer('qty');
+            $table->Integer('selling_price');
+            $table->Integer('buying_price');
+            $table->Integer('product_type_id');
+            $table->enum('product_status', ['available','unvailable'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_03_06_075822_create_sellings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sellings', function (Blueprint $table) {
+            $table->id();
+            $table->BigInteger('customer_id');
+            $table->BigInteger('cashier_id');
+            $table->Date('date_sell');
+            $table->enum('product_status', ['delivery','not_delivery'])->default('not_delivery');
+            $table->Integer('grand_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sellings');
+    }
+};
